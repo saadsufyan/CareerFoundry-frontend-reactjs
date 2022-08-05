45dfcc6e3b1173936083198ea255e87cd53128f3
saad <saadsufyan19@gmail.com>
2022-08-06 00:11:14 +0500

    code saver

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react"
+import Calendar from "react-calendar"
+import "./App.css"
+import Time from "./Components/Time.js"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const tileDisabled = ({ activeStartDate, date, view }) => {
+  return date <= new Date()
 }
 
-export default App;
+function App() {
+  const [date, setDate] = useState(new Date())
+  const [showTime, setShowTime] = useState(false)
+
+  return (
+    <>
+    <h1 className="header">React Calendar</h1>
+    <div className="app">
+      <div className="calendar__main">
+        <div>
+          <Calendar
+            onChange={setDate}
+            value={date}
+            onClickDay={() => setShowTime(true)}
+            tileDisabled={tileDisabled}
+          />
+        </div>
+
+        {date.length > 0 ? (
+          <p>
+            <span>Start:</span>
+            {date[0].toDateString()}
+            &nbsp; &nbsp;
+            <span>End:</span>
+            {date[1].toDateString()}
+          </p>
+        ) : (
+          <p>
+            <span>Default selected date:</span>
+            {date.toDateString()}
+          </p>
+        )}
+        </div>
+      <Time showTime={showTime} date={date} />
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Time.js
@@ -0,0 +1,117 @@
+import React from "react"
+import { useState, useEffect } from "react"
+import { Modal, Button } from "react-bootstrap"
+import ReactTooltip from "react-tooltip"
+import "../App.css"
+import * as moment from 'moment'
+import timeObj from './Data/data.json'
+
+let format="DD-MM-YYYY"
+
+
+function Times(props) {
+  const [event, setEvent] = useState(null)
+  const [info, setInfo] = useState(false)
+  const [value, setValue] = useState(false)
+  const [message, setMessage] = useState("")
+  const [slotIndex, setSlotIndex] = useState()
+  const [currDate, setCurrDate] = useState(moment(props.date.toDateString()).format(format) )
+  const isCookieEnabled = navigator.cookieEnabled;
+  const handleChange = (event) => {
+    setMessage(event.target.value)
+  }
+
+  const addReason = (index,date) => {
+    timeObj[date][index].reason = message
+    timeObj[date][index].isBooked = true
+    timeObj[date][index].date = date.toDateString()
+    setMessage("")
+  }
+
+  function displayInfo(e, value) {
+    if (!value) {
+      setInfo(true)
+    } else {
+      setInfo(false)
+      
+    }
+    setEvent(e.target.innerText)
+  }
+
+  function modalOpen(indexValue) {
+    if (value) {
+      return (
+        <Modal
+          show={value}
+          onHide={() => {
+            setValue(false)
+          }}
+        >
+          <Modal.Header>
+            <Modal.Title>Enter Information</Modal.Title>
+          </Modal.Header>
+          <form>
+            <Modal.Body>
+              <h2>Enter the reason for the meeting</h2>
+              <input
+                type="text"
+                placeholder="Enter the reason for the meeting"
+                style={{ width: "100%" }}
+                id="message"
+                name="message"
+                onChange={handleChange}
+                value={message}
+              ></input>
+            </Modal.Body>
+
+            <Modal.Footer>
+              <Button
+                variant="secondary"
+                onClick={() => {
+                  setValue(false)
+                  addReason(indexValue,moment(props.date.toDateString()).format(format) )
+                }}
+              >
+                Confirm Call
+              </Button>
+            </Modal.Footer>
+          </form>
+        </Modal>
+      )
+    }
+  }
+
+  return (
+    <div className="times">
+      {timeObj[moment(props.date.toDateString()).format(format)]?.map((times, index) => {
+        return (
+          <div className="slot_booking" key={index}>
+            <button
+              data-tip={times.isBooked ? "Slot already booked" : ""}
+              className={times.isBooked ? "button-29 disabled " : "button-29"}
+              onClick={(e) => {
+                displayInfo(e, times.isBooked)
+                setValue(!times.isBooked)
+                setSlotIndex(index)
+              }}
+              // disabled={times.isBooked ? true : false}
+            >
+              {times.time}
+            </button>
+            <ReactTooltip />
+            <span>{times.isBooked ? "Booked" : "Free Slot"}</span>
+            <span>Reason: {!times.isBooked ? "" : times.reason}</span>
+            {<div>{modalOpen(slotIndex)}</div>}
+          </div>
+        )
+      })}
+      <div>
+        {info
+          ? `Your appointment is set to ${event} ${props.date.toDateString()}`
+          : null}
+      </div>
+    </div>
+  )
+}
+
+export default Times
